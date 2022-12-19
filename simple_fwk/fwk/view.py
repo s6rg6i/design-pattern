607e43b298aa140a5386725521a06f2eca8146dc
Sergey Karnaukhov <s_karn_v@hotmail.com>
2022-12-19 12:27:09 +0300

    WSGI-server: added load static files

--- simple_fwk/fwk/view.py
+++ simple_fwk/fwk/view.py
@@ -1,9 +1,5 @@
-from __future__ import annotations
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from fwk.request import RequestData
-    from fwk.response import ResponseData
+from fwk.request import RequestData
+from fwk.response import ResponseData
 
 
 class BaseView:
@@ -12,9 +8,3 @@ class BaseView:
 
     def post(self, request: RequestData, *args, **kwargs) -> ResponseData:
         pass
-
-
-class StaticFile(BaseView):
-    def get(self, request: RequestData, *args, **kwargs):
-        body = f'Загрузка файла {request.path_info}'
-        return ResponseData(request, body=body)
