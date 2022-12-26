9b719192dd2b54dc5badd14d6523b7d2d88d1a76
Sergey Karnaukhov <s_karn_v@hotmail.com>
2022-12-26 13:52:43 +0300

    WSGI-server: creational patterns

--- simple_fwk/views.py
+++ simple_fwk/views.py
@@ -4,9 +4,13 @@ from datetime import datetime
 
 if TYPE_CHECKING:
     from fwk.request import RequestData
+from patterns.creational import Engine
 from fwk.response import ResponseData
 from fwk.jinja import render
 from fwk.view import BaseView
+
+engine = Engine()
+engine.load_test_data()  # загрузка тестовых данных
 
 
 class Index(BaseView):
@@ -35,4 +39,48 @@ class About(BaseView):
 
     def post(self, request: RequestData, *args, **kwargs):
         body = render('about.html', post=f'Получен запрос POST: {request.POST}')
+        return ResponseData(request, body=body)
+
+
+class Adm(BaseView):
+    def get(self, request: RequestData, *args, **kwargs):
+        body = render('adm.html')
+        return ResponseData(request, body=body)
+
+
+class AddCategory(BaseView):
+    def get(self, request: RequestData, *args, **kwargs):
+        body = render('adm_add_ctg.html')
+        return ResponseData(request, body=body)
+
+    def post(self, request: RequestData, *args, **kwargs):
+        ctg = request.POST.get('name', [''])[0]
+        if engine.create_category(ctg):
+            return ResponseData(request, body=render('adm_add_ctg.html', msg=f'Создана новая категория: {ctg}'))
+        return ResponseData(request, body=render('adm_add_ctg.html', msg=f'Ошибка создания категории: {ctg}'))
+
+
+class AddCourse(BaseView):
+    def get(self, request: RequestData, *args, **kwargs):
+        fmts = engine.get_learning_formats()
+        ctgs = engine.categories
+        body = render('adm_add_course.html', formats=fmts, categories=ctgs)
+        return ResponseData(request, body=body)
+
+    def post(self, request: RequestData, *args, **kwargs):
+        ctg = request.POST.get('categories', [''])[0]
+        name = request.POST.get('name', [''])[0]
+        fmt = request.POST.get('format', [''])[0]
+        fmts = engine.get_learning_formats()
+        ctgs = engine.categories
+        if engine.create_course(ctg, name, fmt):
+            msg = f'Создан новый курс: {name}'
+            return ResponseData(request, body=render('adm_add_course.html', formats=fmts, categories=ctgs, msg=msg))
+        msg = f'Ошибка создания курса: {name}'
+        return ResponseData(request, body=render('adm_add_course.html', formats=fmts, categories=ctgs, msg=msg))
+
+
+class ShowCourses(BaseView):
+    def get(self, request: RequestData, *args, **kwargs):
+        body = render('adm_show_courses.html', categories=engine.categories)
         return ResponseData(request, body=body)
